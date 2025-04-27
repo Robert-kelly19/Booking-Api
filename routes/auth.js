dffd0ace89be40575016891b1a2e1162b0e1456d
akuma19 <kellyhandy31@gmail.com>
2025-04-27 12:19:04 +0100

    user login working smoothly

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,11 @@ import express from 'express'
 import userRegisterHandler from '../controller/userRegister-controller.js'
 import { validate, userLoginvalidator } from '../validator/user-validator.js'
 import Auth from '../middlewares/Authmiddleware.js'
+import loginHandler from '../controller/userLogin-controller.js'
 
 const router = express.Router()
 router.post("/register",validate, userRegisterHandler )
+
+router.post("/login", userLoginvalidator, loginHandler)
 
 export default router
